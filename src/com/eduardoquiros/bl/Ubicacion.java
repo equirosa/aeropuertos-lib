b822ba210cd0fac86b2582db28bedac2f8a22c4e
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-07-18 16:07:44 -0600

    Crear clase Ubicación.

---
+++ src/com/eduardoquiros/bl/Ubicacion.java
@@ -0,0 +1,48 @@
+package com.eduardoquiros.bl;
+
+public class Ubicacion {
+    private String codigo, nombre;
+
+    public Ubicacion() {
+    }
+
+    public Ubicacion(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ubicacion ubicacion = (Ubicacion) o;
+
+        return getCodigo().equals(ubicacion.getCodigo());
+
+    }
+
+    @Override
+    public String toString() {
+        return "Ubicacion{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
