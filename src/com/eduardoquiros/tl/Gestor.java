c7b21185bd937e6480a8ef5a28f24b6bd8b1b9db
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-07-23 16:51:11 -0600

    Agregar funcionalidad de guardado de pais al gestor.

--- src/com/eduardoquiros/tl/Gestor.java
+++ src/com/eduardoquiros/tl/Gestor.java
@@ -1,4 +1,13 @@
 package com.eduardoquiros.tl;
 
+import com.eduardoquiros.bl.Pais;
+import com.eduardoquiros.dl.Logica;
+
 public class Gestor {
+    private Logica CL = new Logica();
+
+    private void guardarPais(String codigo, String nombre, String abreviatura){
+        Pais tmpPais = new Pais(codigo,nombre,abreviatura);
+        CL.guardar(tmpPais);
+    }
 }
