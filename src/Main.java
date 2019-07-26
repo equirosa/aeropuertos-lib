01b0ee4dd2678d14087e3822b49853ebef773c9c
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-07-25 18:16:45 -0600

    Iniciar solicitud en Main.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
+import com.eduardoquiros.tl.Gestor;
+
 public class Main {
+
+    private static Gestor controlador;
+
+    public static void main(String[] args){
+        if(verificarExistenciaAdmin()){
+
+        }
+    }
+
+    private static boolean verificarExistenciaAdmin() {
+        return controlador.verirficarExistenciaAdmin();
+    }
 }
