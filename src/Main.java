85cb26a052b571b814c096a3bd2b967815b5c2eb
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-07-25 18:34:55 -0600

    Agregar metodos de input/output.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
 import com.eduardoquiros.tl.Gestor;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 
+public class Main {
+    private static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    private static PrintStream out = System.out;
     private static Gestor controlador;
 
     public static void main(String[] args){
         do {
             if (verificarExistenciaAdmin()) {
 
+            }else{
+                solicitarPrimerAdmin();
             }
         }while(!(verificarExistenciaAdmin()));
+    }
+
+    private static void solicitarPrimerAdmin() {
+
     }
 
     private static boolean verificarExistenciaAdmin() {
