d55890f617e4c07d0b5a460a58bdaf2116e3089f
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 14:23:27 -0600

    Eliminar signature de excepcion sql de Conector.

--- src/com/eduardoquiros/accesodatos/Conector.java
+++ src/com/eduardoquiros/accesodatos/Conector.java
@@ -1,11 +1,9 @@
 package com.eduardoquiros.accesodatos;
 
-import java.sql.SQLException;
-
 public class Conector {
 	private static AccesoBD conectorBD = null;
 	
-	public static AccesoBD getConector() throws SQLException,Exception{
+	public static AccesoBD getConector() throws Exception{
 		if(conectorBD == null){
 			conectorBD = new AccesoBD("com.mysql.cj.jdbc.Driver","jdbc:mysql://localhost/apartados?","root","");
 		}
