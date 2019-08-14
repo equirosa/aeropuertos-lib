d5c517fc5869445c29836d336d65e37385b6363f
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 14:35:57 -0600

    Eliminar newline sobrante.

--- src/com/eduardoquiros/bl/dao/admin/IAdminDao.java
+++ src/com/eduardoquiros/bl/dao/admin/IAdminDao.java
@@ -6,11 +6,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public interface IAdminDao {
-	void insertar(String nombre, String apellido1, String apellido2, String cedula, String email, String direccion,
-	              Pais nacionalidad, LocalDate fechaNacimiento, char genero) throws Exception;
+	void insertar(String nombre, String apellido1, String apellido2, String cedula, String email, String direccion, Pais nacionalidad, LocalDate fechaNacimiento, char genero) throws Exception;
 	ArrayList<Admin> getAdmins() throws Exception;
 	void eliminar(String codigo)throws Exception;
-	void modificar(String nombre, String apellido1, String apellido2, String cedula, String email, String direccion,
-	               Pais nacionalidad, LocalDate fechaNacimiento, char genero) throws Exception;
+	void modificar(String nombre, String apellido1, String apellido2, String cedula, String email, String direccion, Pais nacionalidad, LocalDate fechaNacimiento, char genero) throws Exception;
 	Admin buscarPorCodigo(String codigo) throws Exception;
 }
