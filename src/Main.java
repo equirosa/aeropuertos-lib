cb4fdbde3beaa680c310d6d01007d9f88040ce8c
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 17:50:06 -0600

    Agregar prueba de buscar pais con codigo.

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public static ControladorPais gestorPais = new ControladorPais();
 
     public static void main(String[] args){
         System.out.println(gestorPais.insertar("123","Costa Rica","CRC"));
+        System.out.println(gestorPais.buscarPorCodigo("123"));
     }
 }
