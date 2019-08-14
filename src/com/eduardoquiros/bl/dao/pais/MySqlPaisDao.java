1c337472b3e78ef68fd7b6d3f710ff4cf9ff497a
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 15:52:25 -0600

    Agregar busqueda de paises por codigo.

---
+++ src/com/eduardoquiros/bl/dao/pais/MySqlPaisDao.java
@@ -0,0 +1,35 @@
+package com.eduardoquiros.bl.dao.pais;
+
+import com.eduardoquiros.accesodatos.Conector;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class MySqlPaisDao implements IPaisDao{
+	@Override
+	public void insertar(String codigo, String nombre, String abreviatura) throws Exception {
+		Conector.getConector().ejecutarSql("insert into pais(codigo,nombre,abreviatura) values('"+codigo+"','"+nombre+"','"+abreviatura+"');");
+	}
+	
+	@Override
+	public ArrayList<Pais> getPaises() throws Exception {
+		return null;
+	}
+	
+	@Override
+	public void eliminar(String codigo) throws Exception {
+	
+	}
+	
+	@Override
+	public void modificar(String codigo, String nombre, String abreviatura) throws Exception {
+	
+	}
+	
+	@Override
+	public Pais buscarPorCodigo(String codigo) throws Exception {
+		ResultSet rs =Conector.getConector().ejecutarQuery("select codigo,nombre,abreviatura from pais where codigo='"+codigo+"';");
+		Pais tmpPais = new Pais(rs.getString("codigo"),rs.getString("nombre"),rs.getString("abreviatura"));
+		return tmpPais;
+	}
+}
