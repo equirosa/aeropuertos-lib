806d398500116943169a8aca9a2422a224eb9943
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 15:46:44 -0600

    Crear interface dao de paises.

---
+++ src/com/eduardoquiros/bl/dao/pais/IPaisDao.java
@@ -0,0 +1,11 @@
+package com.eduardoquiros.bl.dao.pais;
+
+import java.util.ArrayList;
+
+public interface IPaisDao {
+	void insertar(String codigo, String nombre, String abreviatura) throws Exception;
+	ArrayList<Pais> getPaises() throws Exception;
+	void eliminar(String codigo)throws Exception;
+	void modificar(String codigo, String nombre, String abreviatura) throws Exception;
+	Pais buscarPorCodigo(String codigo) throws Exception;
+}
