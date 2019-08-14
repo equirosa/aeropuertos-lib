89a6fc5755751345fd6e69a5017798405fa9ed4c
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 17:32:48 -0600

    Rellenar MySqlDaoFactory para retornar los daos de los objetos.

--- src/com/eduardoquiros/bl/dao/factory/MySqlDaoFactory.java
+++ src/com/eduardoquiros/bl/dao/factory/MySqlDaoFactory.java
@@ -1,4 +1,12 @@
 package com.eduardoquiros.bl.dao.factory;
 
+import com.eduardoquiros.bl.dao.admin.IAdminDao;
+import com.eduardoquiros.bl.dao.admin.MySqlAdminDao;
+import com.eduardoquiros.bl.dao.pais.IPaisDao;
+import com.eduardoquiros.bl.dao.pais.MySqlPaisDao;
+
 public class MySqlDaoFactory extends DaoFactory {
+	public IPaisDao getPaisDao(){return new MySqlPaisDao();}
+	
+	public IAdminDao getAdminDao(){return new MySqlAdminDao();}
 }
