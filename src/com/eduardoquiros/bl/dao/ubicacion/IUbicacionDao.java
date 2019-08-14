448846e70b15d6672fd2b78b3393d5dedcc6328d
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 13:41:14 -0600

    Agregar comandos de sql para ubicaciones.

--- src/com/eduardoquiros/bl/dao/ubicacion/IUbicacionDao.java
+++ src/com/eduardoquiros/bl/dao/ubicacion/IUbicacionDao.java
@@ -4,6 +4,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface IUbicacionDao {
-	void insertar(String codigo, String nombre, String aeropuerto)throws SQLException;
-	ArrayList<Ubicacion> getUbicaciones() throws SQLException;
+	void insertar(String codigo, String nombre, String aeropuerto) throws Exception;
+	ArrayList<Ubicacion> getUbicaciones() throws Exception;
 }
---
+++ src/com/eduardoquiros/bl/dao/ubicacion/MySqlUbicacionDao.java
@@ -0,0 +1,28 @@
+package com.eduardoquiros.bl.dao.ubicacion;
+
+import com.eduardoquiros.accesodatos.Conector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class MySqlUbicacionDao implements IUbicacionDao{
+	
+	@Override
+	public void insertar(String codigo, String nombre, String aeropuerto) throws Exception {
+		String queryText = "insert into ubicacion(codigo,codigo_aeropuerto,nombre) values('"+codigo+"','"+aeropuerto+"','"+nombre+"')";
+		Conector.getConector().ejecutarSql(queryText);
+	}
+	
+	@Override
+	public ArrayList<Ubicacion> getUbicaciones() throws Exception {
+		ArrayList<Ubicacion> ubicaciones = new ArrayList<>();
+		String queryText = "select codigo,codigo_aeropuerto,nombre from articulo";
+		ResultSet rs = Conector.getConector().ejecutarQuery(queryText);
+		while (rs.next()) {
+			Ubicacion tmpUbicacion = new Ubicacion(rs.getString("codigo"),rs.getString("codigo_aeropuerto"),rs.getString("nombre"));
+			ubicaciones.add(tmpUbicacion);
+		}
+		return ubicaciones;
+	}
+}
