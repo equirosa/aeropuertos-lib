b5a6feaacabd2be8fb2f2766961946392acdf2d1
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 16:17:42 -0600

    Crear clase que presenta mensajes predeterminados.

---
+++ src/com/eduardoquiros/utils/messages.java
@@ -0,0 +1,6 @@
+package com.eduardoquiros.utils;
+
+public class messages {
+	public static final String MNSJ_EXITO = "Operacion ejecutada exitosamente.";
+	public static final String MNSJ_REG_REPETIDO = "El registro ya existe el la base de datos.";
+}
