95534abfe262295930ef27348999907785793dc5
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 12:52:00 -0600

    Agregar dao factory.

---
+++ src/com/eduardoquiros/bl/dao/factory/DaoFactory.java
@@ -0,0 +1,21 @@
+package com.eduardoquiros.bl.dao.factory;
+
+public abstract class DaoFactory {
+	public static final int MySQL = 1;
+	public static final int SQL = 2;
+	public static final int Oracle = 3;
+	
+	public static DaoFactory getDaoFactory(int whichFactory){
+		switch(whichFactory) {
+			case 1:
+				return new MySqlDaoFactory();
+			case 2:
+//				return new SqlServerDaoFactory();
+			break;
+			case 3:
+//				return new OracleDaoFactory();
+			break;
+		}
+		return null;
+	}
+}
