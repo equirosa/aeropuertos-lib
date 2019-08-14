e50e38867a647d2de3c2cc4ca7c78653c6ced332
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-14 17:24:07 -0600

    Eliminar comandos obsoletos en Main e importar nuevo controlador.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import com.eduardoquiros.bl.dao.admin.Admin;
+import com.eduardoquiros.tl.ControladorPais;
 
 public class Main {
-    private static Controlador controlador;
-
+public static ControladorPais gestorPais = new ControladorPais();
     public static void main(String[] args){
-        Admin blabla = new Admin();
-        System.out.println(blabla.getClass());
     }
 }
