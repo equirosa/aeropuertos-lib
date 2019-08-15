b17c1ea469ef89698f3e9868dcb116ac1f2df64a
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-15 00:58:25 -0600

    Agregar import de Nuevo controlador.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import com.eduardoquiros.tl.ControladorPais;
 
 public class Main {
 public static ControladorPais gestorPais = new ControladorPais();
+//public static ControladorAdmin gestorAdmin = new ControladorAdmin();
 
     public static void main(String[] args){
         System.out.println(gestorPais.insertar("123","Costa Rica","CRC"));
-//        System.out.println(gestorPais.buscarPorCodigo("123"));
+        System.out.println(gestorPais.buscarPorCodigo("123"));
+        System.out.println(ges);
     }
 }
