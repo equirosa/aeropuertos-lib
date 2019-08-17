46e45707d3fa3b0819296b44aa258c6b6e49907f
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-16 22:04:05 -0600

    Comentar funciones no implementadas, postergando.

--- src/com/eduardoquiros/bl/dao/factory/SqlServerDaoFactory.java
+++ src/com/eduardoquiros/bl/dao/factory/SqlServerDaoFactory.java
@@ -1,11 +1,7 @@
 package com.eduardoquiros.bl.dao.factory;
 
-import com.eduardoquiros.bl.dao.admin.IAdminDao;
-import com.eduardoquiros.bl.dao.pais.IPaisDao;
-import com.eduardoquiros.bl.dao.ubicacion.IUbicacionDao;
-
 public class SqlServerDaoFactory {
-	public IAdminDao getAdminDao(){return new SqlServerAdminDao();}
-	public IUbicacionDao getUbicacionDao(){return new SqlServerUbicacionDao();}
-	public IPaisDao getPaisDao(){return new SqlServerPaisDao();}
+//	public IAdminDao getAdminDao(){return new SqlServerAdminDao();}
+//	public IUbicacionDao getUbicacionDao(){return new SqlServerUbicacionDao();}
+//	public IPaisDao getPaisDao(){return new SqlServerPaisDao();}
 }
