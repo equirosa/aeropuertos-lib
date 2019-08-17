dca281628fd90e0cf198b715194ba678bba5f66c
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-16 22:02:47 -0600

    Agregar interface de Ubicacion a dao de MySQL.

--- src/com/eduardoquiros/bl/dao/factory/MySqlDaoFactory.java
+++ src/com/eduardoquiros/bl/dao/factory/MySqlDaoFactory.java
@@ -4,9 +4,11 @@ import com.eduardoquiros.bl.dao.admin.IAdminDao;
 import com.eduardoquiros.bl.dao.admin.MySqlAdminDao;
 import com.eduardoquiros.bl.dao.pais.IPaisDao;
 import com.eduardoquiros.bl.dao.pais.MySqlPaisDao;
+import com.eduardoquiros.bl.dao.ubicacion.IUbicacionDao;
+import com.eduardoquiros.bl.dao.ubicacion.MySqlUbicacionDao;
 
 public class MySqlDaoFactory extends DaoFactory {
 	public IPaisDao getPaisDao(){return new MySqlPaisDao();}
-	
+	public IUbicacionDao getUbicacionDao(){return new MySqlUbicacionDao();}
 	public IAdminDao getAdminDao(){return new MySqlAdminDao();}
 }
