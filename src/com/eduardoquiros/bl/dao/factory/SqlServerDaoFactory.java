4d8920c59c40bfeaf139f13d3764b7d60f70c4df
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-16 22:02:13 -0600

    Agregar funciones basicas similares a las de MySQL.

--- src/com/eduardoquiros/bl/dao/factory/SqlServerDaoFactory.java
+++ src/com/eduardoquiros/bl/dao/factory/SqlServerDaoFactory.java
@@ -1,4 +1,11 @@
 package com.eduardoquiros.bl.dao.factory;
 
+import com.eduardoquiros.bl.dao.admin.IAdminDao;
+import com.eduardoquiros.bl.dao.pais.IPaisDao;
+import com.eduardoquiros.bl.dao.ubicacion.IUbicacionDao;
+
 public class SqlServerDaoFactory {
+	public IAdminDao getAdminDao(){return new SqlServerAdminDao();}
+	public IUbicacionDao getUbicacionDao(){return new SqlServerUbicacionDao();}
+	public IPaisDao getPaisDao(){return new SqlServerPaisDao();}
 }
