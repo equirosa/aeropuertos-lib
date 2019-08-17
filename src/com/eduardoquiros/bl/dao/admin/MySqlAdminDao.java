a2e4b2610acc196afb6b0aceadf4e223726c3cf0
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-16 21:18:53 -0600

    Implementar funciones de Admin.

--- src/com/eduardoquiros/bl/dao/admin/MySqlAdminDao.java
+++ src/com/eduardoquiros/bl/dao/admin/MySqlAdminDao.java
@@ -36,16 +36,25 @@ public class MySqlAdminDao implements IAdminDao{
 	
 	@Override
 	public void eliminar(String codigo) throws Exception {
-	
+	Conector.getConector().ejecutarSql("delete from admin where cedula="+codigo+";");
 	}
 	
 	@Override
-	public void modificar(String nombre, String apellido1, String apellido2, String cedula, String email, String direccion, Pais nacionalidad, LocalDate fechaNacimiento, char genero) throws Exception {
-	
+	public void modificar(String nombre, String apellido1, String apellido2, String cedula, String email, String direccion,
+	                      Pais nacionalidad, LocalDate fechaNacimiento, char genero) throws Exception {
+	Conector.getConector().ejecutarSql("update admin set nombre="+nombre+",apellido1="+apellido1+",apellido2="+apellido2+"," +
+			"email="+email+",direccion="+direccion+",nacionalidad="+nacionalidad.getCodigo()+",fecha_nacimiento="+fechaNacimiento+"," +
+			"genero="+genero+" where cedula="+cedula+";");
 	}
 	
 	@Override
 	public Admin buscarPorCodigo(String codigo) throws Exception {
-		return null;
+		ResultSet rs = Conector.getConector().ejecutarQuery("select cedula,nombre,apellido1,apellido2,email,direccion," +
+				"nacionalidad,fecha_nacimiento,genero where cedula="+codigo+";");
+		return new Admin(rs.getString("nombre"),rs.getString("apellido1"),
+				rs.getString("apellido2"),rs.getString("cedula"),rs.getString("email"),
+				rs.getString("direccion"),gestorPaises.buscarPorCodigo(rs.getString("nacionalidad")),
+				LocalDate.parse(rs.getString("fecha_nacimiento")),rs.getInt("edad"),
+				rs.getString("genero").charAt(0));
 	}
 }
