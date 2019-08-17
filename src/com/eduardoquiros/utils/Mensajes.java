522f3b8bc130db72cddaad4be9a49eef70726b25
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-17 01:44:24 -0600

    Agreagr nuevo codigo de error.

--- src/com/eduardoquiros/utils/Mensajes.java
+++ src/com/eduardoquiros/utils/Mensajes.java
@@ -5,4 +5,5 @@ public class Mensajes {
 	public static final String MNSJ_REG_REPETIDO = "El registro ya existe el la base de datos.";
 	public static final String MNSJ_NO_DEFINIDO = "Error.";
 	public static final String MNSJ_MALA_SINTAXIS = "Comando SQL mal escrito.";
+	public static final String MNSJ_COLUMNA_MALA = "La columna especificada no se encuentra en la tabla.";
 }
