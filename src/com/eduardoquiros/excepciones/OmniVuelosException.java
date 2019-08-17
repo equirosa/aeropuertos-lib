acce8926c318d9388f1628237377df4abb09a54d
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-17 11:55:17 -0600

    Agregar nuevos codigos de error.

--- src/com/eduardoquiros/excepciones/OmniVuelosException.java
+++ src/com/eduardoquiros/excepciones/OmniVuelosException.java
@@ -22,6 +22,8 @@ public class OmniVuelosException extends Exception{
 			case 1054: return Mensajes.MNSJ_COLUMNA_MALA;
 			case 1062: return Mensajes.MNSJ_REG_REPETIDO;
 			case 1064: return Mensajes.MNSJ_MALA_SINTAXIS;
+			case 1146: return Mensajes.MNSJ_TABLA_MALA;
+			case 2006: return Mensajes.MNSJ_SERVIDOR_CAIDO;
 		}
 	}
 }
--- src/com/eduardoquiros/utils/Mensajes.java
+++ src/com/eduardoquiros/utils/Mensajes.java
@@ -6,4 +6,6 @@ public class Mensajes {
 	public static final String MNSJ_NO_DEFINIDO = "Error.";
 	public static final String MNSJ_MALA_SINTAXIS = "Comando SQL mal escrito.";
 	public static final String MNSJ_COLUMNA_MALA = "La columna especificada no se encuentra en la tabla.";
+	public static final String MNSJ_SERVIDOR_CAIDO = "El servidor no se puede encontrar.";
+	public static final String MNSJ_TABLA_MALA = "La tabla especificada no existe";
 }
