b34f70eb9aa8c84529ef8cba35380b45176695d7
Eduardo Quirós <equirosa@ucenfotec.ac.cr>
2019-08-19 14:07:31 -0600

    Crear llamada de verificacion de existencia de Admins en Controlador.

--- src/com/eduardoquiros/tl/ControladorLogin.java
+++ src/com/eduardoquiros/tl/ControladorLogin.java
@@ -1,5 +1,21 @@
 package com.eduardoquiros.tl;
 
-public class ControladorLogin {
+import com.eduardoquiros.bl.dao.admin.MySqlAdminDao;
+import com.eduardoquiros.excepciones.OmniVuelosException;
 
+import java.sql.SQLException;
+
+public class ControladorLogin {
+private MySqlAdminDao adminDao;
+
+	public boolean checkAdmins(){
+		try {
+			return adminDao.getAdmins().equals(null);
+		} catch (SQLException e) {
+			return new OmniVuelosException(e.getErrorCode()).numeroToString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
